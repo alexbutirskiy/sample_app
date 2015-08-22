916a90f32f4815eeb65942aa32b6b65e9fbf2b90
Alex Butirskiy <butirskiy@gmail.com>
2015-08-22 18:07:15 +0300

    7.1 finished

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe "User Pages" do
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -10,3 +18,4 @@ describe "User Pages" do
     it { should have_title(full_title('Sign up')) }
   end
 end
+
